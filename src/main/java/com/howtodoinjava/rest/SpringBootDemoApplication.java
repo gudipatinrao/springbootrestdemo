8df1688c37d244177a9e6597ce24b485ebcde86a
gudipatinrao <narasimharao.gudipati@ibm.com>
2021-01-21 16:45:18 -0600

    initial

---
+++ src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.howtodoinjava.rest;
+/*http://localhost:8080/employees/ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication 
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
